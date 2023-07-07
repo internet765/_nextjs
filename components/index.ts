d4b512e95973c7a2a68a61edd32a229d5e69be14
Гагара Николай Федорович <gagara.nikolai@mspu.edu.ru>
2023-07-07 16:08:09 +0300

    работа с сортировкой

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,9 @@
-export {Htag} from './Htag/Htag';
-export {Button} from './Button/Button';
-export {Tag} from './Tag/Tag';
-export {Rating} from './Rating/Rating';
-export {P} from './P/P';
-export {Card} from './Card/Card';
-export {HhData} from './HhData/HhData';
-export {Advantages} from './Advantages/Advantages'
+export { Htag } from "./Htag/Htag";
+export { Button } from "./Button/Button";
+export { Tag } from "./Tag/Tag";
+export { Rating } from "./Rating/Rating";
+export { P } from "./P/P";
+export { Card } from "./Card/Card";
+export { HhData } from "./HhData/HhData";
+export { Advantages } from "./Advantages/Advantages";
+export { Sort } from "./Sort/Sort";
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { TopPageComponentProps } from "./TopPageComponent.props";
-import { Htag, Tag, HhData, Advantages, P } from "../../components";
+import { Htag, Tag, HhData, Advantages, Sort } from "../../components";
 import style from "./TopPageComponent.module.css";
 import { TopLevelGategory } from "../../interfaces/page.interface";
+import { SortEnum } from "../../components/Sort/Sort.props";
+import { sortReducer } from "./sort.reducer";
 
 function TopPageComponent({
   products,
   firstCategory,
   page,
 }: TopPageComponentProps): JSX.Element {
+  const [{ products: sortProducts, sort }, dispatchSort] = useReducer(sortReducer, {
+    products,
+    sort: SortEnum.Rating,
+  });
+
+  const setSort = (sort: SortEnum) => {
+    dispatchSort({ type: sort });
+  };
+
   return (
     <div className={style.wrapper}>
       <div className={style.title}>
@@ -16,25 +27,39 @@ function TopPageComponent({
         <Tag color='grey' size='middle'>
           {products && products.length}
         </Tag>
-        <span>Сортировка</span>
+        <Sort sort={sort} setSort={setSort} />
       </div>
       <div>
-      {products && products.map(p=>(
-        <div key={p._id}>{p.title}</div>
-      ))}
+        {sortProducts &&
+          sortProducts.map((p) => <div key={p._id}>{p.title}</div>)}
       </div>
       <div className={style.hhTitle}>
         <Htag tag='h2'>Вакансии - {page.category}</Htag>
-        <Tag color='red' size='middle'>hh.ru</Tag>
+        <Tag color='red' size='middle'>
+          hh.ru
+        </Tag>
       </div>
-      {firstCategory === TopLevelGategory.Courses && page.hh && <HhData {...page.hh}/>}
-      {page.advantages && page.advantages.length > 0 && <>
-        <Htag tag='h2'>Приемущества</Htag>
-        <Advantages advantages={page.advantages}/>
-      </>}
-      {page.seoText && <P>{page.seoText}</P>}
+      {firstCategory === TopLevelGategory.Courses && page.hh && (
+        <HhData {...page.hh} />
+      )}
+      {page.advantages && page.advantages.length > 0 && (
+        <>
+          <Htag tag='h2'>Приемущества</Htag>
+          <Advantages advantages={page.advantages} />
+        </>
+      )}
+      {page.seoText && (
+        <div
+          className={style.seo}
+          dangerouslySetInnerHTML={{ __html: page.seoText }}
+        />
+      )}
       <Htag tag='h2'>Получаемые навыки</Htag>
-      {page.tags.map(t => (<Tag color="primary" key={t}>{t}</Tag>))}
+      {page.tags.map((t) => (
+        <Tag color='primary' key={t}>
+          {t}
+        </Tag>
+      ))}
     </div>
   );
 }
